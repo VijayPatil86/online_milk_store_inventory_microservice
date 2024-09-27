a32a514f641728b02e301c5c2936d00f2af65f12
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-27 17:55:31 +0530

    added @CrossOrigin annotation

--- src/main/java/com/online_milk_store/inventory_service/controller/InventoryController.java
+++ src/main/java/com/online_milk_store/inventory_service/controller/InventoryController.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.online_milk_store.inventory_service.service.InventoryService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/inventory-service/inventory")
 public class InventoryController {
