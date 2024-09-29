8edef67e39684e8c981ee11de94f5b8ce1b7bd05
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-29 18:45:06 +0530

    removed method to check inventory status for selected products

--- src/main/java/com/online_milk_store/inventory_service/repository/MilkBrandInventoryRepository.java
+++ src/main/java/com/online_milk_store/inventory_service/repository/MilkBrandInventoryRepository.java
@@ -1,11 +1,8 @@
 package com.online_milk_store.inventory_service.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.online_milk_store.inventory_service.entity.MilkBrandInventoryEntity;
 
 public interface MilkBrandInventoryRepository extends JpaRepository<MilkBrandInventoryEntity, Integer> {
-	List<MilkBrandInventoryEntity> findAllByMilkBrandEntityMilkBrandIdIn(List<Integer> milkBrandIds);
 }
